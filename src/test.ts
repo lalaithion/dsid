52b60d15fe9bccf095cb3905d3f870d6353ef44f
Izaak Weiss <izaak.weiss@gmail.com>
2023-09-23 15:29:00 -0500

    Use pettier to format

--- src/test.ts
+++ src/test.ts
@@ -1,74 +1,71 @@
-import { Dict } from './index';
+import { Dict } from "./index";
 
 function perf_object() {
-    const characters = 'abcdefghijklmnopqrst';
+  const characters = "abcdefghijklmnopqrst";
 
-    for (let a = 2; a < characters.length; a += 2) {
-        
+  for (let a = 2; a < characters.length; a += 2) {
     let start = Date.now();
     let keyCounter = new Set();
 
     let x: any = {};
 
     for (let i = 0; i < 500; i++) {
-        for (let j = 0; j < 500; j++) {
-            let key = characters.slice(i % a, (j/a) % a);
-            keyCounter.add(key)
-            x[key] = i+j
-        }
+      for (let j = 0; j < 500; j++) {
+        let key = characters.slice(i % a, (j / a) % a);
+        keyCounter.add(key);
+        x[key] = i + j;
+      }
     }
 
     let duration = Date.now() - start;
     console.log(`keys=${keyCounter.size} \t object \t ${duration}`);
-    }
+  }
 }
 
 function perf_react() {
-    const characters = 'abcdefghijklmnopqrst';
+  const characters = "abcdefghijklmnopqrst";
 
-    for (let a = 2; a < characters.length; a += 2) {
-            
+  for (let a = 2; a < characters.length; a += 2) {
     let start = Date.now();
     let keyCounter = new Set();
 
     let x: any = {};
 
     for (let i = 0; i < 500; i++) {
-        for (let j = 0; j < 500; j++) {
-            let key = characters.slice(i % a, (j/a) % a);
-            keyCounter.add(key)
-            let tmp = {...x}
-            tmp[key] = i+j
-            x = tmp
-        }
+      for (let j = 0; j < 500; j++) {
+        let key = characters.slice(i % a, (j / a) % a);
+        keyCounter.add(key);
+        let tmp = { ...x };
+        tmp[key] = i + j;
+        x = tmp;
+      }
     }
 
     let duration = Date.now() - start;
     console.log(`keys=${keyCounter.size} \t react \t ${duration}`);
-}
+  }
 }
 
 function perf_dict() {
-    const characters = 'abcdefghijklmnopqrst';
+  const characters = "abcdefghijklmnopqrst";
 
-
-    for (let a = 2; a < characters.length; a += 2) {
+  for (let a = 2; a < characters.length; a += 2) {
     let start = Date.now();
     let keyCounter = new Set();
 
     let x: Dict<number> = new Dict();
 
     for (let i = 0; i < 500; i++) {
-        for (let j = 0; j < 500; j++) {
-            let key = characters.slice(i % a, (j/a) % a);
-            keyCounter.add(key)
-            x = x.set(key, i+j)
-        }
+      for (let j = 0; j < 500; j++) {
+        let key = characters.slice(i % a, (j / a) % a);
+        keyCounter.add(key);
+        x = x.set(key, i + j);
+      }
     }
 
     let duration = Date.now() - start;
     console.log(`keys=${keyCounter.size} \t dict \t ${duration}`);
-}
+  }
 }
 
 perf_object();
